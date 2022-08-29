82843d9db09daa7c15d5b9973e6f77f725c0d1e1
Gilles <gilles.ferrand31@gmail.com>
2022-08-29 21:40:04 +0200

    Add Home lib and routing

---
+++ libs/fithelper-front/home/feature/src/lib/home/home.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fithelper-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+})
+export class HomeComponent {}
