0332b0cb42d4a4086f044a6431d91b7fae0cb216
nico <Nicolas.RIOT@Fittingbox.com>
2022-09-15 21:43:23 +0200

    refactor(crud wieght): update

--- apps/api/src/modules/weight/services/weight.service.ts
+++ apps/api/src/modules/weight/services/weight.service.ts
@@ -5,6 +5,7 @@ import { Repository } from 'typeorm';
 import { Users } from '../../user/entities/users.entity';
 import { CreateWeightDto } from '../dto/create-weight.dto';
 import { WeightOutputDto } from '../dto/output-weight.dto';
+import { UpdateWeightDto } from '../dto/update-weight.dto';
 import { Weight } from '../entities/weights.entity';
 
 @Injectable()
@@ -23,7 +24,7 @@ export class WeightService {
       throw new NotFoundException('user not found ! ');
     }
     const newWeight = this.weightRepository.create(weight);
-    newWeight.user = user
+    newWeight.user = user;
     // const weights = user.weight ? user.weight : [];
 
     const saveWeight = await this.weightRepository.save(newWeight);
@@ -32,5 +33,32 @@ export class WeightService {
     //   weight: weights.concat(newWeight),
     // });
     return saveWeight;
+  }
+
+  async update(
+    weightId: string,
+    weight: UpdateWeightDto,
+    id: string
+  ): Promise<WeightOutputDto> {
+    const user = await this.userRepository.findOneBy({ id });
+
+    if (!user) {
+      throw new NotFoundException('user not found ! ');
+    }
+    console.log('user weight', user);
+    const weightdb = await this.weightRepository.findOne({
+      where: { id: weightId },
+      relations: ['user'],
+    });
+    console.log(weightdb);
+    if (weightdb.user.id !== user.id) {
+      throw new NotFoundException('user weight and weight not match');
+    }
+
+    const newWeight = {
+      ...weightdb,
+      ...weight,
+    };
+    return this.weightRepository.save(newWeight);
   }
 }
--- apps/api/src/modules/weight/weight.controller.ts
+++ apps/api/src/modules/weight/weight.controller.ts
@@ -1,20 +1,34 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Param, Patch, Post, Put, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { ApiOperation } from '@nestjs/swagger';
 import { GetCurrentUserById } from '../../utils';
 import { CreateWeightDto } from './dto/create-weight.dto';
 import { WeightOutputDto } from './dto/output-weight.dto';
+import { UpdateWeightDto } from './dto/update-weight.dto';
 import { WeightService } from './services/weight.service';
 
 @Controller('weight')
 export class WeightController {
-  constructor(private weightService: WeightService) { }
-  
+  constructor(private weightService: WeightService) {}
+
   @ApiOperation({ summary: 'Create weight' })
   @UseGuards(AuthGuard('jwt'))
   @Post()
-  create(@Body() weight: CreateWeightDto,@GetCurrentUserById() id: string ): Promise<WeightOutputDto> {
-    console.log(id);
-    return this.weightService.create(weight,id);
+  create(
+    @Body() weight: CreateWeightDto,
+    @GetCurrentUserById() id: string
+  ): Promise<WeightOutputDto> {
+    return this.weightService.create(weight, id);
+  }
+
+  @ApiOperation({ summary: 'update weight' })
+  @UseGuards(AuthGuard('jwt'))
+  @Patch(':id')
+  update(
+    @Param('id') weightId : string,
+    @Body() weight: UpdateWeightDto,
+    @GetCurrentUserById() id: string
+  ): Promise<WeightOutputDto> {
+    return this.weightService.update(weightId,weight, id);
   }
 }
