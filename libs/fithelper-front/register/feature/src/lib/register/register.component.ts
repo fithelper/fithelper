207e386aff1e5e9022d3f23f090fe74342e4e4e7
Gilles Ferrand <gilles.ferrand31@gmail.com>
2022-09-18 10:24:27 +0200

    Init register form

--- libs/fithelper-front/register/feature/src/lib/register/register.component.ts
+++ libs/fithelper-front/register/feature/src/lib/register/register.component.ts
@@ -1,15 +1,31 @@
-import {Component} from '@angular/core';
-import {RegisterService} from "@fithelper/fithelper-front/register/data-access";
-import {Observable} from "rxjs";
+import { Component } from '@angular/core';
+import { FormControl, NonNullableFormBuilder, Validators } from '@angular/forms';
+
+import { RegisterService } from "@fithelper/fithelper-front/register/data-access";
 
 @Component({
   selector: 'fithelper-register',
   templateUrl: './register.component.html',
 })
 export class RegisterComponent {
-    constructor(private readonly registerService: RegisterService) { }
+    constructor(private readonly fb: NonNullableFormBuilder, private readonly registerService: RegisterService) { }
 
-    register(email: string, password: string): void {
-      this.registerService.register(email, password).subscribe();
+    registerForm = this.fb.group({
+        email: new FormControl<string>('', [Validators.required, Validators.email]),
+        password: new FormControl<string>('', [Validators.required, Validators.minLength(8)])
+    });
+
+    get email() { return this.registerForm.get('email'); }
+
+    get password() { return this.registerForm.get('password'); }
+
+    register(): void {
+      // Get form values
+      const email = this.registerForm.get('email')?.value;
+      const password = this.registerForm.get('password')?.value;
+      console.warn(email, password);
+      if (email && password) {
+        this.registerService.register(email, password).subscribe();
+      }
     }
 }
