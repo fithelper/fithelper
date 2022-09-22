353b25c276022ff3964d4ca775101909b8b62afb
Gilles Ferrand <gilles.ferrand31@gmail.com>
2022-09-22 21:50:36 +0200

    Finally typed this form correctly

--- libs/fithelper-front/register/feature/src/lib/register/register.component.ts
+++ libs/fithelper-front/register/feature/src/lib/register/register.component.ts
@@ -1,24 +1,22 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, NonNullableFormBuilder, Validators } from '@angular/forms';
+import { FormControl, FormGroup, NonNullableFormBuilder, Validators } from '@angular/forms';
 
 import { RegisterService } from "@fithelper/fithelper-front/register/data-access";
-
-interface RegisterForm {
-  email: FormControl<string>;
-  password: FormControl<string>;
-}
 
 @Component({
   selector: 'fithelper-register',
   templateUrl: './register.component.html',
 })
 export class RegisterComponent {
-    registerForm!: FormGroup<RegisterForm>;
+    registerForm!: FormGroup<{
+      email: FormControl<string>;
+      password: FormControl<string>;
+    }>;
 
-    constructor(private readonly fb: FormBuilder, private readonly registerService: RegisterService) {
+    constructor(private readonly fb: NonNullableFormBuilder, private readonly registerService: RegisterService) {
       this.registerForm = this.fb.group({
-        email: new FormControl<string>('', {validators: [Validators.required, Validators.email], nonNullable: true}),
-        password: new FormControl<string>('', {validators: [Validators.required, Validators.minLength(8)], nonNullable: true}),
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required, Validators.minLength(8)]],
       });
     }
 
